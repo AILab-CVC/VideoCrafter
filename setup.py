4090016910d23d28baa412e71e921cc022c74783
scutpaul <jszxchx@126.com>
2023-10-11 15:53:20 +0800

    Remove all code !!!

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='lvdm',
-    version='0.0.1',
-    description='generic video generation models',
-    packages=find_packages(),
-    install_requires=[],
-)
